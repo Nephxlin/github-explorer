f6fc660fd339b96fd9f7ff6e61c45485c729716b
Luther <luther.m.cordeiro@gmail.com>
2020-09-02 05:17:59 -0300

    Api Github 1.0

---
+++ src/styles/global.ts
@@ -0,0 +1,32 @@
+import {createGlobalStyle} from 'styled-components'
+
+import gitBackground from '../assets/git-background.svg'
+
+export default createGlobalStyle`
+  *{
+    margin:0;
+    padding:0;
+    outline:0;
+    box-sizing: border-box;
+  }
+
+  body{
+    background: #f0f0f5 url(${gitBackground}) no-repeat 70% top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+  }
+
+  #root{
+    max-width: 968px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+`;
